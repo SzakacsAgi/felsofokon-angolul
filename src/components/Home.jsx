480ea3404c433f943f00771debb70d04ac9eccbb
SzakacsAgi <szakacs.agi21@gmail.com>
2024-03-05 17:24:26 +0100

    Create and style homepage services section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,41 @@
 import classes from './Home.module.css';
 import DOMPurify from 'dompurify';
 import {useHeaderContext} from "../store/contexts-provider";
+import SectionTitle from "./sectionTitle";
 
 function isErrorOccurred(pageContent){
     return typeof pageContent === "string";
+}
+
+function getInnerHtml(content){
+    return {__html: DOMPurify.sanitize(content)}
 }
 
 export default function Home({pageContent}){
     const {currentLanguage} = useHeaderContext();
     const isError = isErrorOccurred(pageContent);
     const aboutMeSectionContent = isError ? pageContent : pageContent.aboutMe[currentLanguage];
+    const servicesSectionContent = isError ? pageContent : pageContent.services[currentLanguage];
 
     return (
         <>
             <section className={classes.section}>
                 <div className={classes.para}>
-                    <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(aboutMeSectionContent)}}/>
+                    <div dangerouslySetInnerHTML={getInnerHtml(aboutMeSectionContent)}/>
                 </div>
                 <div className={classes.img}/>
+            </section>
+            <section className={classes.section}>
+                <SectionTitle className={classes.sectionTitle} title={servicesSectionContent.title}/>
+                <div className={classes.servicesContainer}>
+                    {servicesSectionContent.data.map(service =>{
+                        return <div className={classes.serviceCard}>
+                            <div dangerouslySetInnerHTML={getInnerHtml(service.serviceTitle)}></div>
+                            <div dangerouslySetInnerHTML={getInnerHtml(service.description)}></div>
+                            <a href={service.navigation}>{servicesSectionContent.detailsText}</a>
+                        </div>
+                    })}
+                </div>
             </section>
         </>
     )
--- src/data/static-data.jsx
+++ src/data/static-data.jsx
@@ -42,5 +42,75 @@ export const DUMMY_HOMEPAGE_DATA = {
         HUN: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Bemutatkozás:</span></h3>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Szia! Örülök, hogy itt vagy a weboldalamon. Engedd meg, hogy bemutatkozzam neked.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Az én nevem <span style=\"font-weight: bold;\">Anna Tanár</span>, és lelkes magántanárként a célom, hogy segítsek neked elérni a tanulási célokat és felfedezni az adott tantárgy izgalmas világát.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Rendelkezem több éves tapasztalattal az oktatás terén, és személyre szabott tanulási módszereket alkalmazok, hogy segítsem diákjaimat a fejlődésben és a sikerek elérésében.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Az óráimon hangsúlyt fektetek a kölcsönös kommunikációra és a pozitív tanulási környezet kialakítására. Mindig nyitott vagyok a kérdésekre, és elkötelezett vagyok az iránt, hogy támogassalak az utadon.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Ha érdeklődsz a tanulás iránt, és szeretnél segítséget kapni, ne habozz felvenni velem a kapcsolatot. Alig várom, hogy együtt elinduljunk a tanulás izgalmas útján!</p>",
         FR: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Introduction :</span></h3>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Salut ! Je suis ravi de t'avoir sur mon site web. Permettez-moi de me présenter.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Je m'appelle <span style=\"font-weight: bold;\">Anna Enseignante</span>, et en tant que tutrice privée passionnée, mon objectif est de t'aider à atteindre tes objectifs d'apprentissage et d'explorer le monde passionnant de la matière en question.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Avec plusieurs années d'expérience dans l'éducation, j'utilise des méthodes d'enseignement personnalisées pour aider mes étudiants dans leur croissance et leur réussite.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Lors de mes cours, j'insiste sur la communication mutuelle et la création d'un environnement d'apprentissage positif. Je suis toujours ouvert aux questions et engagé à te soutenir dans ton parcours.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Si tu es intéressé(e) par l'apprentissage et que tu cherches de l'aide, n'hésite pas à me contacter. J'ai hâte de nous lancer ensemble sur le chemin passionnant de l'apprentissage !</p>",
         ENG: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Introduction:</span></h3>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">Hello! I'm delighted to have you on my website. Let me introduce myself to you.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">My name is <span style=\"font-weight: bold;\">Anna Teacher</span>, and as a passionate private tutor, my goal is to help you achieve your learning objectives and explore the exciting world of the subject at hand.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">With several years of experience in education, I employ personalized teaching methods to assist my students in their growth and success.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">During my lessons, I emphasize mutual communication and creating a positive learning environment. I'm always open to questions and committed to supporting you on your journey.</p>\n<p style=\"font-family: 'book antiqua', palatino, serif;\">If you're interested in learning and seeking assistance, don't hesitate to get in touch with me. I look forward to embarking on the exciting path of learning together!</p>"
+    },
+    services:{
+        HUN:{
+            title: "Szolgáltatásaim",
+            detailsText: "Részletek",
+            data: [
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Általános Nyelvórák</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Indulj el a nyelvmesterség útján személyre szabott órákkal, amelyek a szintednek megfelelnek. A szókincsépítéstől a nyelvtan alapjain át az óráink minden aspektusát lefedik a teljes körű tanulási élmény érdekében.</p>",
+                    navigation: "general-english-lesson"
+                },
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Professzionális Fordítási Szolgáltatások</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Pontos és megbízható fordításra van szükséged? Szakértői fordítási szolgáltatást nyújtok dokumentumokhoz, cikkekhez és különféle tartalomtípusokhoz. Biztosítsd a tiszta és pontos kommunikációt választott nyelveden.</p>",
+                    navigation: "translation"
+                },
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Matura Vizsgára Felkészítés</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Szerezz kiemelkedő eredményt a Matura vizsgán célzott felkészüléssel. Az összes vizsgarészre kiterjedően a szakosodott programunk a kulcsképességekre, stratégiákra és gyakorlati anyagokra összpontosít a magabiztosságod és sikerességed növelése érdekében a nagy napra.</p>",
+                    navigation: "matura-examination"
+                }
+            ],
+        },
+        ENG:{
+            title: "My Services",
+            detailsText: "Details",
+            data: [
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">General Language Lessons</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Embark on a journey of language mastery with personalized lessons tailored to your proficiency level. From vocabulary building to grammar essentials, our general lessons cover all aspects of the language for a comprehensive learning experience.</p>",
+                    navigation: "general-english-lesson"
+                },
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Professional Translation Services</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Need accurate and reliable translations? I provide expert translation services for documents, articles, and various content types. Ensure clear and precise communication in the language of your choice.</p>",
+                    navigation: "translation"
+                },
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Matura Examination Prep</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Ace your Matura examination with targeted preparation. Covering all sections of the exam, our specialized program focuses on key skills, strategies, and practice materials to boost your confidence and success on the big day.</p>",
+                    navigation: "matura-examination"
+                }
+            ],
+        },
+        FR:{
+            title: "Mes Services",
+            detailsText: "détails",
+            data: [
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Cours de Langue Généraux</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Embarquez pour un voyage de maîtrise linguistique avec des cours personnalisés adaptés à votre niveau de compétence. Du renforcement du vocabulaire aux bases de la grammaire, nos cours généraux couvrent tous les aspects de la langue pour une expérience d'apprentissage complète.</p>",
+                    navigation: "general-french-lesson"
+                },
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Services de Traduction Professionnelle</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Besoin de traductions précises et fiables ? Je propose des services de traduction experts pour les documents, les articles et différents types de contenus. Assurez une communication claire et précise dans la langue de votre choix.</p>",
+                    navigation: "translation"
+                },
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Préparation à l'Examen du Baccalauréat</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Réussissez votre examen du baccalauréat avec une préparation ciblée. Couvrant toutes les sections de l'examen, notre programme spécialisé se concentre sur les compétences clés, les stratégies et les supports d'entraînement pour renforcer votre confiance et assurer votre succès le jour J.</p>",
+                    navigation: "matura-examination"
+                },
+                {
+                    serviceTitle: "<h3><span style=\"font-family: 'book antiqua', palatino, serif;\">Préparation à l'Examen du Baccalauréat</span></h3>",
+                    description: "<p style=\"font-family: 'book antiqua', palatino, serif;\">Réussissez votre examen du baccalauréat avec une préparation ciblée. Couvrant toutes les sections de l'examen, notre programme spécialisé se concentre sur les compétences clés, les stratégies et les supports d'entraînement pour renforcer votre confiance et assurer votre succès le jour J.</p>",
+                    navigation: "matura-examination"
+                }
+            ],
+        }
     }
 }
