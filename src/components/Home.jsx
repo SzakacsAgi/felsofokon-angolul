5c935b014752d67bff61f908bcaa5d3a1fe6ec0a
Agi <szakacs.agi21@gmail.com>
2024-03-07 15:48:11 +0100

    Create and style homepage services section (#5)
    Create and style homepage services section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,41 @@
 import classes from './Home.module.css';
 import DOMPurify from 'dompurify';
 import {useHeaderContext} from "../store/contexts-provider";
+import SectionTitle from "./SectionTitle";
 
 function isErrorOccurred(pageContent){
     return typeof pageContent === "string";
+}
+
+function sanitizeHTMLContent(content){
+    return {__html: DOMPurify.sanitize(content)}
 }
 
 export default function Home({pageContent}){
     const {currentLanguage} = useHeaderContext();
     const isError = isErrorOccurred(pageContent);
     const aboutMeSectionContent = isError ? pageContent : pageContent.aboutMe[currentLanguage];
+    const servicesSectionContent = isError ? pageContent : pageContent.services[currentLanguage];
 
     return (
         <>
             <section className={classes.section}>
                 <div className={classes.para}>
-                    <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(aboutMeSectionContent)}}/>
+                    <div dangerouslySetInnerHTML={sanitizeHTMLContent(aboutMeSectionContent)}/>
                 </div>
                 <div className={classes.img}/>
+            </section>
+            <section className={classes.section}>
+                <SectionTitle className={classes.sectionTitle} title={servicesSectionContent.title}/>
+                <div className={classes.servicesContainer}>
+                    {servicesSectionContent.data.map(service =>{
+                        return <div className={classes.serviceCard}>
+                            <div dangerouslySetInnerHTML={sanitizeHTMLContent(service.serviceTitle)}></div>
+                            <div dangerouslySetInnerHTML={sanitizeHTMLContent(service.description)}></div>
+                            <a href={service.navigation}>{servicesSectionContent.detailsText}</a>
+                        </div>
+                    })}
+                </div>
             </section>
         </>
     )
