30ecbd647403fc5f8c1e8ad349f3b727756f9fba
Ágnes Szakács <szakacs.agi21@gmail.com>
2024-05-11 07:55:11 +0200

    Make the header responsive (#25)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,48 @@
 import './Header.css'
 import NavItem from "./NavItem";
 import FlagNavItem from "./FlagNavItem";
-import {HEADER_TEXT} from "../../data/static-data";
-import {useHeaderContext} from "../../store/contexts-provider";
+import { HEADER_TEXT } from "../../data/static-data";
+import { useHeaderContext } from "../../store/contexts-provider";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-export default function Header(){
-
-    const {currentLanguage} = useHeaderContext();
+export default function Header() {
+    const { currentLanguage } = useHeaderContext();
     const currentLanguageText = HEADER_TEXT[currentLanguage];
 
-    return (
-        <header>
-            <nav className="navbar navbar-expand-lg">
-                <div className="container">
-                    <h1 className="navbar-brand">{currentLanguageText.title}</h1>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
-                        <ul className="navbar-nav">
-                            <NavItem pageName="home">{currentLanguageText.home}</NavItem>
-                            <NavItem pageName="about">{currentLanguageText.about}</NavItem>
-                            <NavItem pageName="services" menuPoints={currentLanguageText.menuPoints}>{currentLanguageText.services}</NavItem>
-                            <NavItem pageName="prices">{currentLanguageText.prices}</NavItem>
-                            <NavItem pageName="contact">{currentLanguageText.contact}</NavItem>
-                            <NavItem pageName="blog">{currentLanguageText.blog}</NavItem>
-                            <FlagNavItem/>
-                        </ul>
+    return <header>
+        <div className='edge-container'>
+            <Navbar expand='lg'>
+                <Container fluid>
+                    <div className='title-container flex-item'>
+                        <h1 href="/home">{currentLanguageText.title}</h1>
                     </div>
-                </div>
-            </nav>
-        </header>
-    )
+                    <Navbar.Toggle aria-controls='offcanvasNavbar-expand-lg' />
+                    <Navbar.Offcanvas
+                        id='offcanvasNavbar-expand-lg'
+                        aria-labelledby='offcanvasNavbarLabel-expand-lg'
+                        placement="end"
+                        className='flex-item'
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id='offcanvasNavbarLabel-expand-lg'><FlagNavItem /></Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1">
+                                <NavItem pageName="home">{currentLanguageText.home}</NavItem>
+                                <NavItem pageName="about">{currentLanguageText.about}</NavItem>
+                                <NavItem pageName="services">{currentLanguageText.services}</NavItem>
+                                <NavItem pageName="prices">{currentLanguageText.prices}</NavItem>
+                                <NavItem pageName="contact">{currentLanguageText.contact}</NavItem>
+                                <NavItem pageName="blog">{currentLanguageText.blog}</NavItem>
+                                <FlagNavItem />
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        </div>
+    </header>
 }
