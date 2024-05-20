2c4888a14a68308ef8e72b9289fc5956a6527004
SzakacsAgi <szakacs.agi21@gmail.com>
2024-05-20 19:42:20 +0200

    Make the header fully responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,42 +7,56 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useState } from 'react';
 
 export default function Header() {
     const { currentLanguage } = useHeaderContext();
     const currentLanguageText = HEADER_TEXT[currentLanguage];
 
-    return <header>
-        <div className='edge-container'>
-            <Navbar expand='lg'>
-                <Container fluid>
-                    <div className='title-container flex-item'>
-                        <h1 href="/home">{currentLanguageText.title}</h1>
-                    </div>
-                    <Navbar.Toggle aria-controls='offcanvasNavbar-expand-lg' />
-                    <Navbar.Offcanvas
-                        id='offcanvasNavbar-expand-lg'
-                        aria-labelledby='offcanvasNavbarLabel-expand-lg'
-                        placement="end"
-                        className='flex-item'
-                    >
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id='offcanvasNavbarLabel-expand-lg'><FlagNavItem /></Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1">
-                                <NavItem pageName="home">{currentLanguageText.home}</NavItem>
-                                <NavItem pageName="about">{currentLanguageText.about}</NavItem>
-                                <NavItem pageName="services">{currentLanguageText.services}</NavItem>
-                                <NavItem pageName="prices">{currentLanguageText.prices}</NavItem>
-                                <NavItem pageName="contact">{currentLanguageText.contact}</NavItem>
-                                <NavItem pageName="blog">{currentLanguageText.blog}</NavItem>
-                                <FlagNavItem />
-                            </Nav>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-            </Navbar>
-        </div>
-    </header>
+    const [showOffcanvas, setShowOffcanvas] = useState(false);
+    const closeOffcanvas = () => setShowOffcanvas(false);
+    const toggleOffcanvas = () => setShowOffcanvas(prev => !prev);
+
+    return (
+        <header>
+            <div className='edge-container'>
+                <Navbar expand={window.outerWidth > 824}>
+                    <Container fluid>
+                        <div className='title-container flex-item'>
+                            <h1 href="/home">{currentLanguageText.title}</h1>
+                        </div>
+                        <Navbar.Toggle 
+                            aria-controls='offcanvasNavbar-expand-md'
+                            onClick={toggleOffcanvas} 
+                        />
+                        <Navbar.Offcanvas
+                            id='offcanvasNavbar-expand-md'
+                            aria-labelledby='offcanvasNavbarLabel-expand-md'
+                            placement="end"
+                            className='flex-item'
+                            show={showOffcanvas} 
+                            onHide={closeOffcanvas} 
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id='offcanvasNavbarLabel-expand-md'>
+                                    <FlagNavItem />
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1">
+                                    <NavItem pageName="home" onClick={closeOffcanvas}>{currentLanguageText.home}</NavItem>
+                                    <NavItem pageName="about" onClick={closeOffcanvas}>{currentLanguageText.about}</NavItem>
+                                    <NavItem pageName="services" onClick={closeOffcanvas}>{currentLanguageText.services}</NavItem>
+                                    {currentLanguage === "HUN" ? <NavItem pageName="prices" onClick={closeOffcanvas}>{currentLanguageText.prices}</NavItem> : ""}
+                                    <NavItem pageName="contact" onClick={closeOffcanvas}>{currentLanguageText.contact}</NavItem>
+                                    <NavItem pageName="blog" onClick={closeOffcanvas}>{currentLanguageText.blog}</NavItem>
+                                    <FlagNavItem onClick={closeOffcanvas} />
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            </div>
+        </header>
+    );
 }
